ae4de3600733f02ea24b82ec485f853e67cd031f
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-03-04 15:57:40 +0100

    update status of header component

---
+++ alloservice_client/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/components/login/login.component';
+import { RegisterComponent } from './auth/components/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FooterComponent } from './pages/shared/footer/footer.component';
+import { HeaderComponent } from './pages/shared/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    FooterComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [
+    provideClientHydration(),
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
