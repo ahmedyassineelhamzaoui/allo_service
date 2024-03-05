7c52d34c82af6f848f943d01ba4e61b79c3cef2d
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-03-05 13:57:14 +0100

    add verify account component with his style

---
+++ alloservice_client/src/app/auth/components/verifyemail/verifyemail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-verifyemail',
+  templateUrl: './verifyemail.component.html',
+  styleUrl: './verifyemail.component.css'
+})
+export class VerifyemailComponent {
+
+}
