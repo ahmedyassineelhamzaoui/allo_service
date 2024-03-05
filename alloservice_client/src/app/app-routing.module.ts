f9921c174ff57161f58368667ed13cca4941eac6
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-03-05 09:15:00 +0100

    add routing for service component

--- alloservice_client/src/app/app-routing.module.ts
+++ alloservice_client/src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { ServiceComponent } from './pages/service/service.component';
 
 const routes: Routes = [
 
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path:'home' , component: HomeComponent},
   {path:'about',component:AboutComponent},
-  {path:'contact',component:ContactComponent}
+  {path:'contact',component:ContactComponent},
+  {path:'services',component:ServiceComponent}
 ];
 
 @NgModule({
