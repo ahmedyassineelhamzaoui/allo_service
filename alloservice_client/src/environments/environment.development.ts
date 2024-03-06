5e02998d5127baf116399e6215b78a4220fb2507
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-03-06 10:41:28 +0100

    add environments variables

---
+++ alloservice_client/src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    apiURL: 'http://localhost:8080/api/v1/',
+    apiUrlAuth:'http://localhost:8080/api/v1/auth/',
+    apiUrlGoogleAuth:'http://localhost:8080/api/v1/oauth2/google',
+};
---
+++ alloservice_client/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiURL: 'http://localhost:8080/api/v1/',
+    apiUrlAuth:'http://localhost:8080/api/v1/auth/'
+};
