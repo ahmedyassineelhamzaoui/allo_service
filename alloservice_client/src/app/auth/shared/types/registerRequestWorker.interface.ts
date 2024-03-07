fcf513b69e1f4e122d06396e5c3f2ef46afd7612
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-03-07 09:57:52 +0100

    add register request client interface

---
+++ alloservice_client/src/app/auth/shared/types/registerRequestWorker.interface.ts
@@ -0,0 +1,11 @@
+export interface RegisterRequestClientInterface{
+    firstName : string;
+    lastName : string;
+    phone: string;
+    gender: string;
+    location:string;
+    picture: string;
+    cardType:string;
+    email : string;
+    password : string;
+}
