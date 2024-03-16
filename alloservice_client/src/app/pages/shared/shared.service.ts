5b607407ea8cdd06d7673e60ce3c2af8a4da277f
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-03-16 12:52:26 +0000

    create all necessary methods in shared service

--- alloservice_client/src/app/pages/shared/shared.service.ts
+++ alloservice_client/src/app/pages/shared/shared.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { PersistanceService } from '../../auth/shared/services/persistance.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SharedService {
 
-  constructor() { }
+
+  private roles: { role: string, permissions: string[] }[] = [];
+
+  constructor( private persistanceService: PersistanceService) {
+    this.initializeRoles();
+  }
+  initializeRoles(): void {
+      const myToken = this.persistanceService.get('accessToken');
+      if (myToken) {
+        const decodedToken = JSON.parse(atob(myToken!.split('.')[1]));
+        this.setRoles(decodedToken.roles);
+      }
+    }
+  
+  setRoles(roles: { role: string, permissions: string[] }[]): void {
+    this.roles = roles;
+  }
+
+  getRoles(): { role: string, permissions: string[] }[] {
+    return this.roles;
+  }
+
+  hasRole(role: string): boolean {
+    return this.roles.some(r => r.role === role);
+  }
+
+  hasPermission(permission: string): boolean {
+    return this.roles.some(r => r.permissions.includes(permission));
+  }
 }
