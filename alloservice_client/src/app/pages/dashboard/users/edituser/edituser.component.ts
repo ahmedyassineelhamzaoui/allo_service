a1c1981e2a1fa9164b8e17db44f0df0160bc88fc
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-03-16 14:02:14 +0000

    add status array

--- alloservice_client/src/app/pages/dashboard/users/edituser/edituser.component.ts
+++ alloservice_client/src/app/pages/dashboard/users/edituser/edituser.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { UserService } from '../../../shared/user.service';
+import { FormBuilder } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-edituser',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrl: './edituser.component.css'
 })
 export class EdituserComponent {
+  errorMessage: string = '';
+  successMessage: string = '';
 
+  status = [
+    { value: 'ACTIVE', name: 'Active' },
+    { value: 'INACTIVE', name: 'Inactive' },
+  ]
+
+  constructor(
+    private userService: UserService,
+    private dialogRef: MatDialogRef<EdituserComponent>,
+    private fb: FormBuilder,
+  ) { }
+
+  closeEditModal() {
+    this.errorMessage = '';
+    this.successMessage = '';
+    this.dialogRef.close();
+  }
 }
