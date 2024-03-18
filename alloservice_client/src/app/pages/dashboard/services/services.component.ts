2afdf53ebe248ba68f954fcea1f7dd121723aab8
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-03-18 12:25:05 +0000

    add method to handle page event

--- alloservice_client/src/app/pages/dashboard/services/services.component.ts
+++ alloservice_client/src/app/pages/dashboard/services/services.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SharedService } from '../../shared/shared.service';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-services',
@@ -7,6 +8,12 @@ import { SharedService } from '../../shared/shared.service';
   styleUrl: './services.component.css'
 })
 export class ServicesComponent {
+
+  isLoading:boolean = false;
+  errorMessage:string = '';
+  services:any[] = [];
+  pageSize:number = 5;
+  currentPageIndex:number = 0;
 
   constructor(private sharedService:SharedService) { }
 
@@ -16,5 +23,9 @@ export class ServicesComponent {
 
   hasPermission(permission: string): boolean {
     return this.sharedService.getRoles().some(r => r.permissions.includes(permission));
+  }
+  handlePageEvent(pageEvent: PageEvent) {
+    this.currentPageIndex = pageEvent.pageIndex;
+    this.pageSize = pageEvent.pageSize;
   }
 }
