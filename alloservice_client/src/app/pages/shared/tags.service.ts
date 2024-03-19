79d11cac999c9b2e5c546f922d01a4dcbb78b283
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-03-19 14:40:13 +0000

    create method to get all tags

--- alloservice_client/src/app/pages/shared/tags.service.ts
+++ alloservice_client/src/app/pages/shared/tags.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TagInterface } from './types/tag.interface';
+import { environment } from '../../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TagsService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getAllTags(): Observable<TagInterface> {
+    return this.http.get<TagInterface>(environment.apiURL+'tags');
+  }
 }
--- alloservice_client/src/app/pages/shared/types/tagRequest.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface TagRequestInterface {
-    id: string;
-    name: string;
-}
