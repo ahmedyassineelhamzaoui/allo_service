a93e6102dcec557db82a4e436c61aa9c16e176af
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-03-22 23:54:15 +0000

    create layout of details service

--- alloservice_client/src/app/pages/dashboard/services/servicedetails/servicedetails.component.ts
+++ alloservice_client/src/app/pages/dashboard/services/servicedetails/servicedetails.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-servicedetails',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class ServicedetailsComponent {
 
+  constructor(
+    private dialogRef: MatDialogRef<ServicedetailsComponent>
+  ) { }
+  closeModal():void{
+    this.dialogRef.close();
+  }
 }
