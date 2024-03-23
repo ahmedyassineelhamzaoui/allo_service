53fe72ea114fa88d94ae9a065c8e0445ae98324c
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-03-23 01:20:09 +0000

    update user response interface

--- alloservice_client/src/app/pages/shared/types/userResponse.interface.ts
+++ alloservice_client/src/app/pages/shared/types/userResponse.interface.ts
@@ -1,7 +1,9 @@
+import { RoleInterface } from "./role.interface";
+
 export interface UserResponseInterface{
     id: string;
     firstName: string;
     lastName: string;
     email: string;
-    role: string;
+    role: RoleInterface[];
 }
