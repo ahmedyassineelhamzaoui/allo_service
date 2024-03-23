4e13ff210699ee5cabc0a0ff1a2b0030f3834c63
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-03-23 01:16:45 +0000

    create the user response interface

--- alloservice_client/src/app/pages/dashboard/services/servicedetails/servicedetails.component.ts
+++ alloservice_client/src/app/pages/dashboard/services/servicedetails/servicedetails.component.ts
@@ -1,5 +1,6 @@
 import { Component,Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { ServiceService } from '../../../shared/service.service';
 
 @Component({
   selector: 'app-servicedetails',
@@ -7,16 +8,22 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
   styleUrl: './servicedetails.component.css'
 })
 export class ServicedetailsComponent {
-
+  service:any = {};
   constructor(
     private dialogRef: MatDialogRef<ServicedetailsComponent>,
     @Inject(MAT_DIALOG_DATA) public data: { id: string},
-
+    private serviceService: ServiceService
   ) { }
   closeModal():void{
     this.dialogRef.close();
   }
   ngOnInit() {
-    console.log(this.data.id);
-  }
+    this.serviceService.getServiceDetails(this.data.id).subscribe(
+    (response:any)=>{
+      this.service = response.details.service;
+    },
+    (error)=>{
+      console.log(error);
+    }
+  )}
 }
---
+++ alloservice_client/src/app/pages/shared/types/userResponse.interface.ts
@@ -0,0 +1,7 @@
+export interface UserResponseInterface{
+    id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    role: string;
+}
