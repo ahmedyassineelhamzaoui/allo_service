153189b6385e1fe73747a94dba1749bd1c7c99e5
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-03-23 13:05:41 +0000

    create the method openEditTagModal

--- alloservice_client/src/app/pages/dashboard/tags/tags.component.ts
+++ alloservice_client/src/app/pages/dashboard/tags/tags.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { TagInterface } from '../../shared/types/tag.interface';
 import { PageEvent } from '@angular/material/paginator';
+import { MatDialog } from '@angular/material/dialog';
+import { EdittagComponent } from './edittag/edittag.component';
 
 @Component({
   selector: 'app-tags',
@@ -8,15 +10,21 @@ import { PageEvent } from '@angular/material/paginator';
   styleUrl: './tags.component.css'
 })
 export class TagsComponent {
+
   tags :TagInterface[] = [];
   currentPageIndex: number = 0;
   pageSize:number = 5;
+
+  constructor(
+    private dilog:MatDialog
+  ){}
 
   handlePageEvent(event:PageEvent){
     this.currentPageIndex = event.pageIndex;
     this.pageSize = event.pageSize;
   }
   openEditTagModal():void{
-    
+
+      this.dilog.open(EdittagComponent);
   }
 }
