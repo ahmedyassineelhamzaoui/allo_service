9978fb7339d09c50e0f1efe4b19087b5568f2800
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-03-25 11:51:21 +0000

    create the method to display all status

--- alloservice_client/src/app/pages/dashboard/services/editserviceasadmin/editserviceasadmin.component.ts
+++ alloservice_client/src/app/pages/dashboard/services/editserviceasadmin/editserviceasadmin.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
@@ -8,9 +9,20 @@ import { MatDialogRef } from '@angular/material/dialog';
 })
 export class EditserviceasadminComponent {
 
-  constructor(private dialogRef:MatDialogRef<EditserviceasadminComponent>){}
+  serviceStatus = [
+    {name:'pending',value:'PENDING'},
+    {name:'approved',value:'APPROVED'},
+    {name:'rejected',value:'NOT_APPROVED'}
+  ]
+  constructor(
+    private dialogRef:MatDialogRef<EditserviceasadminComponent>,
+    private fb: FormBuilder,
+    ){}
 
   closeEditTagModal():void{
     this.dialogRef.close();
   }
+  form = this.fb.group({
+    serviceStatus: ['PENDING'],
+  });
 }
