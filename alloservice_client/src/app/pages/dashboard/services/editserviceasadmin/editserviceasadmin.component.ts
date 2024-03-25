f468ee2be0863c40b9ff6d1bfb01c77719e45f2c
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-03-25 12:28:29 +0000

    create method to edit service status

--- alloservice_client/src/app/pages/dashboard/services/editserviceasadmin/editserviceasadmin.component.ts
+++ alloservice_client/src/app/pages/dashboard/services/editserviceasadmin/editserviceasadmin.component.ts
@@ -1,6 +1,9 @@
 import { Component, Inject } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { ServiceService } from '../../../shared/service.service';
+import { RequestStatusInterface } from '../../../shared/types/requestStatus.interface';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-editserviceasadmin',
@@ -20,7 +23,9 @@ export class EditserviceasadminComponent {
   constructor(
     private dialogRef:MatDialogRef<EditserviceasadminComponent>,
     private fb: FormBuilder,
+    private serviceService: ServiceService,
     @Inject(MAT_DIALOG_DATA) public data: { id: string,status: string},
+    private toastr:ToastrService
     ){
       this.form = this.fb.group({
         serviceStatus: [this.data.status]
@@ -29,6 +34,21 @@ export class EditserviceasadminComponent {
 
   closeEditTagModal():void{
     this.dialogRef.close();
+  }
+  editServiceStatus():void{
+    const request:RequestStatusInterface = {
+      serviceStatus: this.form.get('serviceStatus')?.value || 'PENDING'
+    }
+    this.serviceService.editServiceAsAdmin(this.data.id,request).subscribe(
+      (response:any)=>{
+        console.log(response);
+        this.toastr.success('Service status updated successfully');
+        this.dialogRef.close();
+      },
+      (error)=>{
+        console.log(error);
+      }
+    )
   }
   
 }
