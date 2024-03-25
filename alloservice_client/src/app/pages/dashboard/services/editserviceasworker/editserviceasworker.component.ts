7c87f697c1392a35e5790a56ce1919a499866bae
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-03-25 14:30:25 +0000

    create the method to get service by id

--- alloservice_client/src/app/pages/dashboard/services/editserviceasworker/editserviceasworker.component.ts
+++ alloservice_client/src/app/pages/dashboard/services/editserviceasworker/editserviceasworker.component.ts
@@ -1,5 +1,9 @@
 import { Component,Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { ServiceService } from '../../../shared/service.service';
+import { ResponseWithDetailsInterface } from '../../../../auth/shared/types/responseWithDetails.interface';
+import {Observable} from 'rxjs';
+import { ServiceResponseInterface } from '../../../shared/types/serviceResponse.interface';
 
 @Component({
   selector: 'app-editserviceasworker',
@@ -8,8 +12,30 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 })
 export class EditserviceasworkerComponent {
 
+  service : ServiceResponseInterface = {
+    id: '',
+    user: {
+      id: '',
+      firstName: '',
+      lastName: '',
+      email: '',
+      phone: '',
+      role: []
+    },
+    title: '',
+    price: 0,
+    servicePicture: {
+      id: '',
+      link: ''
+    },
+    description: '',
+    tags: [],
+    availability: [],
+    serviceStatus: ''
+  };
   constructor(
     private dialogRef:MatDialogRef<EditserviceasworkerComponent>,
+    private serviceService:ServiceService,
     @Inject(MAT_DIALOG_DATA) public data: { id: string}
   ) { }
 
@@ -20,5 +46,14 @@ export class EditserviceasworkerComponent {
   closeEditServiceModal():void{
     this.dialogRef.close();
   }
-
+  getServiceById(id:string){
+      this.serviceService.getServiceById(id).subscribe(
+        (response:any) =>{
+          console.log(response);
+        },
+        (error)=>{
+          console.log(error);
+        }
+      )
+  }
 }
